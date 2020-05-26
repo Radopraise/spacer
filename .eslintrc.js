6f851492f60bd9c2bb75622d22bc17ecfa3a481a
Radoslaw <radoslaw.kwidzinski@gmail.com>
2020-05-26 13:43:48 +0200

    Connect app to NASA API

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,4 @@
+/* eslint-disable */ 
 module.exports = {
   root: true,
   env: {
@@ -10,9 +11,12 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2020,
   },
+  // rules: {
+  //   'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  //   'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  // },
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/max-attributes-per-line': 'warn'
   },
   overrides: [
     {
